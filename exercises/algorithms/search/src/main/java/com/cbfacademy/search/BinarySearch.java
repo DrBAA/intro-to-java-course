88fb5f476bba71cf91fa4a119ed9eaeb9b88f892
DrBAA <muchiribeth@yahoo.co.uk>
2024-06-27 23:20:53 +0100

    edited the BinarySearch algorithm

--- exercises/algorithms/search/src/main/java/com/cbfacademy/search/BinarySearch.java
+++ exercises/algorithms/search/src/main/java/com/cbfacademy/search/BinarySearch.java
@@ -7,12 +7,19 @@ public class BinarySearch implements Search {
 
         int low = 0;
         int high = sortedArray.length-1;
-        int mid = (low+high)/2;
+
+        // iterate until l <= r
+        while (low <= high) {
+            int mid = (low+high)/2;
+
+        // check if target is at mid
+
         
         if(target == sortedArray[mid]){
             return mid;
         }
         
+        // if target greater than mid then ignore left half
         else if (target > sortedArray[mid]) {
             low = mid + 1;
         }
@@ -21,7 +28,15 @@ public class BinarySearch implements Search {
             high = mid - 1;
         }
 
-        return target;
+        // if element not present
+
+        return -1;
         
-    };
+        }
+        return high;
+
+        // return binarySearch.search(sortedArray, target);
+
+    }
+
 }
