5f248e59ff6fd09233a309d07a1360eeaa01daf4
DrBAA <muchiribeth@yahoo.co.uk>
2024-07-27 20:38:43 +0100

    commented out the Circle.java class in object oriented programming exercise to enable run the shapes classes

--- exercises/object-oriented-programming/shapes/src/main/java/com/cbfacademy/shapes/Circle.java
+++ exercises/object-oriented-programming/shapes/src/main/java/com/cbfacademy/shapes/Circle.java
@@ -1,22 +1,22 @@
-package com.cbfacademy.shapes;
+// package com.cbfacademy.shapes;
 
-public class Circle implements Enclosure {
+// public class Circle implements Enclosure {
 
-    double radius;
+//     double radius;
   
-    public Circle (double radius){
-        this.radius = radius;
-    }
+//     public Circle (double radius){
+//         this.radius = radius;
+//     }
 
-    @Override
-    public double perimeter() {
-        return perimeter;        
-    };
+//     @Override
+//     public double perimeter() {
+//         return perimeter;        
+//     };
 
-    @Override
-    public double area(){
-        return area;
-    }
+//     @Override
+//     public double area(){
+//         return area;
+//     }
 
 
-}
+// }
