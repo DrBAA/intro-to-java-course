cecbe9d06c98ec80a79ca16ea6a4fcfcb228e5a1
DrBAA <muchiribeth@yahoo.co.uk>
2024-07-28 20:39:57 +0100

    added ShowroomTest.java to the right folder. run Showroom.java but BUILD FAILURE

--- exercises/object-oriented-programming/cars/src/main/java/com/cbfacademy/cars/App.java
+++ exercises/object-oriented-programming/cars/src/main/java/com/cbfacademy/cars/App.java
@@ -4,8 +4,9 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        Showroom showroom = new Showroom();
-        List<Car> cars = showroom.getCars();
+//        Showroom showroom = new Showroom();
+//        List<Car> cars = showroom.getCars();
+        List<Car> cars = new Showroom().getCars();
 
         for (Car car : cars) {
             System.out.println(car);
