2bac62df5312310050460b8abaf2d37f776234bc
DrBAA <muchiribeth@yahoo.co.uk>
2024-08-06 20:24:32 +0100

    modified SimpleContainer class in software design exercises and added instructions to it. No modification to the code was done

--- exercises/software-design/src/main/java/com/cbfacademy/SimpleContainer.java
+++ exercises/software-design/src/main/java/com/cbfacademy/SimpleContainer.java
@@ -1,3 +1,28 @@
+/**
+ * A simple IoC container that uses a map to store mappings between interfaces
+ * and their implementations.
+ * It provides methods to register mappings, create instances of interfaces, and
+ * retrieve instances of interfaces.
+ */
+// IoC containers exercise
+ //While we've now decoupled our code, we still have to create instances of our interfaces using multiple factory classes. In a real-world application with numerous interfaces defined, this can quickly become a maintenance nightmare. To address this, we can use a IoC Container to manage our dependencies.
+
+// Examine the SimpleContainer class. It may contain code that looks unfamiliar, but focus on the comments describing the behaviour of the register and create methods.
+// 1. Add the following method to the App class:
+//     private static SimpleContainer initialiseContainer() {
+//         SimpleContainer container = new SimpleContainer();
+
+//         // Register mappings for any required interfaces with their concrete implementations
+
+//         return container;
+//     }
+// 1. Modify the initialiseContainer method to register mappings for the Game and Player interfaces with their concrete implementations in the container, e.g. container.register(Game.class, DiceGame.class)
+// 2. Add a call to initialiseContainer in the main method of App, before any factory method calls.
+// 3. Replace the call to GameFactory.create() with container.get(Game.class)
+// 4. Remove the calls to PlayerFactory.create()
+// 5. Run the application again to confirm you get the same output as before.
+// By using a container, we're able to simplify our code and eliminate the need for multiple factory classes. This makes our code more modular, maintainable and easier to understand.
+
 package com.cbfacademy;
 
 import java.lang.reflect.Constructor;
@@ -5,12 +30,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * A simple IoC container that uses a map to store mappings between interfaces
- * and their implementations.
- * It provides methods to register mappings, create instances of interfaces, and
- * retrieve instances of interfaces.
- */
 public class SimpleContainer {
     private final Map<Class<?>, Class<?>> types;
 
