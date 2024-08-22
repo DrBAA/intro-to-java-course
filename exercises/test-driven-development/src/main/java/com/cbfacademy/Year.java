f52b378c9320d1e62ae0e48b425a097656a7922b
Shanice Dunk <shanice.dunk@icloud.com>
2024-08-22 21:11:48 +0100

    Updated code with if statement

--- exercises/test-driven-development/src/main/java/com/cbfacademy/Year.java
+++ exercises/test-driven-development/src/main/java/com/cbfacademy/Year.java
@@ -2,6 +2,12 @@ package com.cbfacademy;
 
 public class Year {
     public static boolean isLeap(int year) {
+        // years divisible by 400 are leap years
+        if (year % 400 == 0) {
+            return true;
+        } else {
+            
+        }
         return false;// year % 400 == 0;
     }
 }
